9abc8f6d06746492a22e597d096eaedda04b23ff
machukhinktato <tarabrinmv@gmail.com>
2020-05-19 16:46:47 +0300

    fixes

--- user/main.py
+++ user/main.py
@@ -1,4 +1,18 @@
-a = list(input('please werite something here: '))
+# a = list(input('please werite something here: '))
 
 
-print(type(a))
+class User:
+    def __init__(self, name, income, balance):
+        self.name = name
+        self.income = income,
+        self.balance = balance
+
+    def __str__(self):
+        return self.__all__()
+
+    def __eq__(self, other):
+        return (
+            self.name == other.name,
+            self.name == other.name,
+            self.name == other.name,
+            )
