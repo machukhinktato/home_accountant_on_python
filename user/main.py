8e35f5cf3ea095b290579b23a3ae91fb9372ad8c
machukhinktato <tarabrinmv@gmail.com>
2020-05-19 16:59:26 +0300

    fixes

--- user/main.py
+++ user/main.py
@@ -4,15 +4,23 @@
 class User:
     def __init__(self, name, income, balance):
         self.name = name
-        self.income = income,
+        self.income = income
         self.balance = balance
 
     def __str__(self):
-        return self.__all__()
+        return f'{self.name} {self.income} {self.balance}'
+    #
+    # def __eq__(self, other):
+    #     return (
+    #         self.name == other.name,
+    #         self.name == other.name,
+    #         self.name == other.name,
+    #         )
 
-    def __eq__(self, other):
-        return (
-            self.name == other.name,
-            self.name == other.name,
-            self.name == other.name,
-            )
+#
+# a = User('Vasya', 1000, '0')
+# print(a.name, a.balance, a.income)
+
+a = User('Vasya', 1000, '0')
+
+print(a)
