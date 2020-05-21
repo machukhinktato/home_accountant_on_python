73890caf8305bc5493b0e638c49a4ad3d29a3f67
machukhinktato <tarabrinmv@gmail.com>
2020-05-21 18:57:11 +0300

    fixes

--- user/main.py
+++ user/main.py
@@ -1,6 +1,3 @@
-from datetime import datetime
-
-
 class User:
     def __init__(self, name, income, balance, target):
         self.name = name
@@ -36,14 +33,30 @@ class Account():
                f'Расходы {self.expenses} \n' \
                f'Доходы {self.income}'
 
-# class Account_Management():
 
+class Account_Management:
+    def __init__(self):
+        self.ex_per_day = self.add_expense()
+        self.ex_per_week = None
+        self.ex_per_month = None
 
+# vex_per_day, ex_per_week, ex_per_month
+    def __str__(self):
+        return f'Расходы за день {self.ex_per_day} \n' \
+               f'Расходы за неделю {self.ex_per_week} \n' \
+               f'Расходы за месяц {self.ex_per_month}'
 
+    def add_expense(self, expense):
+        self.expense = expense
+
+        return self.expense
 
 if __name__ == '__main__':
     a = User('Vasya', 50000, 10000, 5000)
     # b = User.create_user('Misha', 50000, 10000, 5000, 5)
     print(a)
-    c = Account(a.name, 45000, a.income)
+    b = Account(a.name, 45000, a.income)
+    print(b)
+    c = Account_Management()
+    c.add_expense(1)
     print(c)
