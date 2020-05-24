5d4c244b519cae4ec7c7ccaa9ff6457af1811004
machukhinktato <tarabrinmv@gmail.com>
2020-05-24 16:12:57 +0300

    user done , lets create account

--- builder.py
+++ builder.py
@@ -1,28 +1,35 @@
 import abc
 
 
-class UserBuildDirector:
+class HomeBudgetBuildDirector:
     def __init__(self):
         self._builder = None
 
-    def construct(self, builder, name, income, balance, wish_to_save):
+    def construct(self, builder, name, income, balance, wish_to_save, category,
+                  expense, total_gain):
         self._builder = builder
         self._builder._build_name(name)
         self._builder._build_income(income)
         self._builder._build_balance(balance)
         self._builder._build_wish_to_save(wish_to_save)
+        self._builder._build_category(category)
+        self._builder._build_expense(expense)
+        self._builder._build_total_gain(total_gain)
 
 
-class User:
+class HomeBudget:
     name = ''
-    income = 0
-    balance = 0
-    wish_to_save = 0
+    income = []
+    balance = []
+    wish_to_save = []
+    category = {}
+    expense = []
+    total_gain = 0
 
 
 class AbstractTableBuilder(metaclass=abc.ABCMeta):
     def __init__(self):
-        self.person = User()
+        self.person = HomeBudget()
 
     @abc.abstractmethod
     def _build_name(self):
@@ -34,6 +41,18 @@ class AbstractTableBuilder(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def _build_balance(self):
+        pass
+
+    @abc.abstractmethod
+    def _build_category(self):
+        pass
+
+    @abc.abstractmethod
+    def _build_expense(self):
+        pass
+
+    @abc.abstractmethod
+    def _build_total_gain(self):
         pass
 
     @abc.abstractmethod
@@ -54,16 +73,24 @@ class UserBuilder(AbstractTableBuilder):
     def _build_wish_to_save(self, wish_to_save):
         self.person.wish_to_save = wish_to_save
 
+    def _build_category(self, category):
+        self.person.category = category
+
+    def _build_expense(self, expense):
+        self.person.expense = expense
+
+    def _build_total_gain(self, total_gain):
+        self.person.total_gain = total_gain
+
 
 user__builder = UserBuilder()
-director = UserBuildDirector()
-Builder = director.construct
-
+director = HomeBudgetBuildDirector()
 
 if __name__ == '__main__':
-    Builder(user__builder, 'Василий', 1000, 1500, 500)
+    director.construct(user__builder, 'Василий', 1000, 1500, 500, 'trata', 1000, 0)
     Vasya = user__builder.person
     print(f'Пользователь {Vasya.name}, с доходом {Vasya.income} рублей, '
           f'и балансом в настоящий момент {Vasya.balance} рублей,\n'
           f'Хочет по окончанию отчетного периода иметь не менее '
-          f'{Vasya.wish_to_save} рублей')
+          f'{Vasya.wish_to_save} рублей'
+          f'{Vasya.category}, {Vasya.expense}, {Vasya.total_gain}')
