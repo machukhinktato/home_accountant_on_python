9b078007788c44800b4b422bb1d368acec3ac589
machukhinktato <tarabrinmv@gmail.com>
2020-05-24 17:29:09 +0300

    oke

--- account.py
+++ account.py
@@ -1,24 +1,28 @@
 import abc
-from user import User
+from user import *
 
 
-class Account:
-    def __init__(self, name, income):
-        self.name = name
+class Accounts:
+    accounts = []
+
+    def __init__(self, account, income):
+        self.accounts.append(account)
         self.income = income
-        self.expenses = []
 
     def __str__(self):
-        return f'пользователь {self.name} доходы {self.income}'
+        return f'{self.accounts} income {self.income} rubles'
 
     def create_account(owner_name, income):
-        user_account = Account(owner_name, income)
+        user_account = Accounts(owner_name, income)
         return user_account
-
-    def add_expense(user_account):
-        expense =
 
 
 if __name__ == '__main__':
-    a = Account.create_account('Vasya', 1000)
+    Builder(user__builder, 'Василий', 1000, 1500, 500)
+    Vasya = user__builder.person
+    Builder(user__builder, 'Petya', 1000, 1500, 500)
+    Petya = user__builder.person
+    a = Accounts.create_account(Vasya.name, Vasya.income)
+    b = Accounts.create_account(Petya.name, Petya.income)
     print(a)
+    print(b)
