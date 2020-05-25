f7eb806ee592736a390c2f520f98d7444168c1bc
machukhinktato <tarabrinmv@gmail.com>
2020-05-25 14:54:40 +0300

    abstract category added

--- categories.py
+++ categories.py
@@ -1,2 +1,14 @@
-class Categories:
-    pass
+import abc
+
+
+class AbstractCategory(metaclass=abc.ABCMeta):
+    def __init__(self):
+        self.category = Category()
+
+    def set_income(self):
+        pass
+
+    def get_expense(self):
+        pass
+
+
