e282c3b85890921118e00b20a99862f9880f719e
machukhinktato <tarabrinmv@gmail.com>
2020-06-01 14:43:16 +0300

    hot fixes

--- categories.py
+++ categories.py
@@ -1,13 +1,16 @@
 class Category:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, title):
+        self.title = title
         self.expenses = []
 
     def __str__(self):
         return f'{self.title}'
 
-    def __repr__(self):
-        return f'{self.title}'
+    def get_sum(self):
+        return sum(self.expenses)
+
+    def get_expense_list(self):
+        return self.expenses
 
 
 class Expenses:
@@ -16,3 +19,12 @@ class Expenses:
         self.value = value
         self.category.expenses.append(self.value)
 
+
+if __name__ == '__main__':
+    products = Category('Продукты')
+    car = Category('Машина')
+    print(products)
+    expense = Expenses(products, 1000)
+    expense = Expenses(products, 1000)
+    print(products.title, products.get_expense_list())
+    print(car.title, car.get_sum())
