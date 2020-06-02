701300e87eb06a63f8de3f8e52140196d373c557
machukhinktato <tarabrinmv@gmail.com>
2020-06-02 10:36:02 +0300

    user reformated, account in progress

--- account.py
+++ account.py
@@ -3,22 +3,22 @@ from categories import *
 from expenses import *
 
 
-class Account:
-    def __init__(self, user, expenses):
-        self.user = user
-        self.expenses = expenses
-
-
-if __name__ == '__main__':
-    products = Category('Products')
-    car = Category('Car')
-    print(products)
-    expense = Expenses(products, 1000)
-    expense = Expenses(products, 1000)
-    expense = Expenses(car, 5000)
-    expense = Expenses(products, 1000)
-    print(products.get_title(), products.get_expense_list(), products.get_expense_sum())
-    print(car.get_title(), car.get_expense_sum())
-    Builder(user__builder, 'Василий', 1000, 1500, 500)
-    user = user__builder.person
-    print(user.__dict__)
+class FinancialStreams:
+    def __init__(self, category, value):
+        self.category = category
+        self.value = value
+        self.category.fin_stream.append(self.value)
+#
+# if __name__ == '__main__':
+#     products = Category('Products')
+#     car = Category('Car')
+#     print(products)
+#     expense = Expenses(products, 1000)
+#     expense = Expenses(products, 1000)
+#     expense = Expenses(car, 5000)
+#     expense = Expenses(products, 1000)
+#     print(products.get_title(), products.get_expense_list(), products.get_expense_sum())
+#     print(car.get_title(), car.get_expense_sum())
+#     Builder(user__builder, 'Василий', 1000, 1500, 500)
+#     user = user__builder.person
+#     print(user.__dict__)
