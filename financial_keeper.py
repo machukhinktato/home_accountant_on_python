75fab394c5f64da8b5b7fa429910ff85bd57f559
machukhinktato <tarabrinmv@gmail.com>
2020-06-04 12:10:10 +0300

    fixes

--- financial_keeper.py
+++ financial_keeper.py
@@ -9,15 +9,14 @@ class User:
         return f'{self.name}'
 
 
-
 class Category:
-    def __init__(self, name, balance, type_of='expense'):
-        self.name = name
+    def __init__(self, title, balance, type_of='expense'):
+        self.title = title
         self.__balance = balance
         self.main_category = type_of
 
     def __str__(self):
-        return f'{self.name} {self.__balance} {self.main_category}'
+        return f'{self.title} {self.__balance} {self.main_category}'
 
     def __add__(self, other):
         pass
@@ -31,13 +30,9 @@ class Category:
         self.__balance += value
         return self.__balance
 
-    def del_val(self):
-        del self.__balance
+    def annul_val(self):
+        self.__balance = 0
 
 
 if __name__ == '__main__':
     user = User('Misha')
-    products = Category('products', 1000, 'income')
-    print(products)
-    products.del_val()
-    print(products)
