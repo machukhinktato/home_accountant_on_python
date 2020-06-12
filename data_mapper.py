f6bcf3c93f9927b8ecb9353e5a2df62335e63260
machukhinktato <tarabrinmv@gmail.com>
2020-06-12 13:03:25 +0300

    unit work in progress, hot fixes

--- data_mapper.py
+++ data_mapper.py
@@ -1,4 +1,5 @@
 import sqlite3
+import threading
 
 connection = sqlite3.connect('fin_keeper.db')
 
@@ -24,7 +25,6 @@ class DbDeleteException(Exception):
 
 
 class FinancialMapper:
-
     def __init__(self, connection):
         self.connection = connection
         self.cursor = connection.cursor()
@@ -64,6 +64,79 @@ class FinancialMapper:
             self.connection.commit()
         except Exception as e:
             raise DbDeleteException(e.args)
+
+
+class MapperRegistry:
+    @staticmethod
+    def get_mapper(obj):
+        if isinstance(obj, Category):
+            return PersonMapper(connection)
+
+
+class UnitOfWork:
+    current = threading.local()
+
+    def __init__(self):
+        self.new_objects = []
+        self.dirty_objects = []
+        self.removed_objects = []
+
+    def register_new(self, obj):
+        self.new_objects.append(obj)
+
+    def register_dirty(self, obj):
+        self.dirty_objects.append(obj)
+
+    def register_removed(self, obj):
+        self.removed_objects.append(obj)
+
+    def commit(self):
+        self.insert_new()
+        self.update_dirty()
+        self.delete_removed()
+
+    def insert_new(self):
+        for obj in self.new_objects:
+            MapperRegistry.get_mapper(obj).insert(obj)
+
+    def update_dirty(self):
+        for obj in self.dirty_objects:
+            MapperRegistry.get_mapper(obj).update(obj)
+
+    def delete_removed(self):
+        for obj in self.removed_objects:
+            MapperRegistry.get_mapper(obj).delete(obj)
+
+    @staticmethod
+    def new_current():
+        __class__.set_current(UnitOfWork())
+
+    @classmethod
+    def set_current(cls, unit_of_work):
+        cls.current.unit_of_work = unit_of_work
+
+    @classmethod
+    def get_current(cls):
+        return cls.current.unit_of_work
+
+
+class DomainObject:
+
+    def mark_new(self):
+        UnitOfWork.get_current().register_new(self)
+
+    def mark_dirty(self):
+        UnitOfWork.get_current().register_dirty(self)
+
+    def mark_removed(self):
+        UnitOfWork.get_current().register_removed(self)
+
+
+class Person(DomainObject):
+    def __init__(self, id_person, first_name, last_name):
+        self.id_person = id_person
+        self.last_name = last_name
+        self.first_name = first_name
 
 
 class Category:
