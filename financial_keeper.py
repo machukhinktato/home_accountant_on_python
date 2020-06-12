d989ac6b54451d1823235309ac0c7f8214a04015
machukhinktato <tarabrinmv@gmail.com>
2020-06-12 12:57:54 +0300

    unit in work in progress

--- financial_keeper.py
+++ financial_keeper.py
@@ -88,6 +88,78 @@ class FinancialOperator:
         result = financial_mapper.search(self)
         return f'{result.__dict__}'
 
+    class MapperRegistry:
+        @staticmethod
+        def get_mapper(obj):
+            if isinstance(obj, Person):
+                return PersonMapper(connection)
+
+    class UnitOfWork:
+        """
+        Паттерн UNIT OF WORK
+        """
+        # Работает с конкретным потоком
+        current = threading.local()
+
+        def __init__(self):
+            self.new_objects = []
+            self.dirty_objects = []
+            self.removed_objects = []
+
+        def register_new(self, obj):
+            self.new_objects.append(obj)
+
+        def register_dirty(self, obj):
+            self.dirty_objects.append(obj)
+
+        def register_removed(self, obj):
+            self.removed_objects.append(obj)
+
+        def commit(self):
+            self.insert_new()
+            self.update_dirty()
+            self.delete_removed()
+
+        def insert_new(self):
+            for obj in self.new_objects:
+                MapperRegistry.get_mapper(obj).insert(obj)
+
+        def update_dirty(self):
+            for obj in self.dirty_objects:
+                MapperRegistry.get_mapper(obj).update(obj)
+
+        def delete_removed(self):
+            for obj in self.removed_objects:
+                MapperRegistry.get_mapper(obj).delete(obj)
+
+        @staticmethod
+        def new_current():
+            __class__.set_current(UnitOfWork())
+
+        @classmethod
+        def set_current(cls, unit_of_work):
+            cls.current.unit_of_work = unit_of_work
+
+        @classmethod
+        def get_current(cls):
+            return cls.current.unit_of_work
+
+    class DomainObject:
+
+        def mark_new(self):
+            UnitOfWork.get_current().register_new(self)
+
+        def mark_dirty(self):
+            UnitOfWork.get_current().register_dirty(self)
+
+        def mark_removed(self):
+            UnitOfWork.get_current().register_removed(self)
+
+    class Person(DomainObject):
+        def __init__(self, id_person, first_name, last_name):
+            self.id_person = id_person
+            self.last_name = last_name
+            self.first_name = first_name
 
 if __name__ == '__main__':
     user = User('Misha')
