3005d93dfaf969159be996ff033a9ea4259a24d4
machukhinktato <tarabrinmv@gmail.com>
2020-06-17 16:38:34 +0300

    its works, hot fixes in progress

--- app_main_framework.py
+++ app_main_framework.py
@@ -6,19 +6,28 @@ def about_view():
     return '200 OK', '<h1>King, since born</h1>'
 
 urls = {
-    '/', index_view(),
-    '/about', about_view()
+    '/': index_view(),
+    '/about': about_view()
 }
 
 
 def application(environ, start_response):
-    path = environ['PATH_INFO']
-    if path == '/':
-        start_response('200 OK', [('Content-Type', 'text/html')])
-        return [b'Hello from a King Mike, whom trying to be the best']
-    elif path == 'about':
-        start_response('200 OK', [('Content-Type', 'text/html')])
-        return [b"<h1> He's real king</h1>"]
-    else:
-        start_response('404 NOT FOUND', [('Content-Type', 'text/html')])
-        return [b"<h1> He's real king</h1>"]
+    pass
+
+class Application:
+    def __init__(self, urls):
+        self.urls = urls
+
+
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO']
+
+        if path == '/':
+            start_response('200 OK', [('Content-Type', 'text/html')])
+            return [b'Hello from a King Mike, whom trying to be the best']
+        elif path == 'about':
+            start_response('200 OK', [('Content-Type', 'text/html')])
+            return [b"<h1> He's real king</h1>"]
+        else:
+            start_response('404 NOT FOUND', [('Content-Type', 'text/html')])
+            return [b"<h1> He's real king</h1>"]
