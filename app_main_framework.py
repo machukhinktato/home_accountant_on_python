e099847dc753d7f6a18c54977be161c92454e2f1
machukhinktato <tarabrinmv@gmail.com>
2020-06-18 18:15:53 +0300

    hot fixes

--- app_main_framework.py
+++ app_main_framework.py
@@ -1,41 +1,53 @@
-def index_view():
-    return '200 OK', 'Greetings from King Mike'
-
-
-def about_view():
-    return '200 OK', '<h1>King, since born</h1>'
-
-urls = {
-    '/': index_view(),
-    '/about': about_view()
-}
-
-
-def application(environ, start_response):
-    pass
-
 class Application:
-    def __init__(self, urls):
-        self.urls = urls
 
+    def __init__(self, urls, middlewares):
+        self.urls = urls
+        self.middlewares = middlewares
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
 
+        request = {}
+
+        for middleware in self.middlewares:
+            middleware(request)
+
         if path in self.urls:
             view = self.urls[path]
-            code, text = view()
-            start_response('200 OK', [('Content-Type', 'text/html')])
+            code, text = view(request)
+            start_response(code, [('Content-Type', 'text/html')])
             return [text.encode(encoding='utf-8')]
         else:
-            return [b'not Found']
+            start_response('404 WHAT HAPPEND', [('Content-Type', 'text/plain')])
+            return [b'Not Found']
 
-        if path == '/':
-            start_response('200 OK', [('Content-Type', 'text/html')])
-            return [b'Hello from a King Mike, whom trying to be the best']
-        elif path == 'about':
-            start_response('200 OK', [('Content-Type', 'text/html')])
-            return [b"<h1> He's real king</h1>"]
-        else:
-            start_response('404 NOT FOUND', [('Content-Type', 'text/html')])
-            return [b"<h1> He's real king</h1>"]
+
+# Установка
+# pip install uwsgi
+
+# Запуск
+# uwsgi --http :8000 --wsgi-file simple_wsgi_server.py
+def index_view(request):
+    return '200 OK', 'Torpedo Moscow CHAMPION!!!'
+
+
+def about_view(request):
+    if 'secret' in request:
+        return '200 OK', f'<h1>About Page {request["secret"]}</h1>'
+    return '200 OK', f'<h1>About Page</h1>'
+
+
+urls = {
+    '/': index_view,
+    '/about/': about_view
+}
+
+
+def secret_middleware(request):
+    request['secret'] = 'secret'
+
+
+# middlewares = [secret_middleware]
+middlewares = []
+
+application = Application(urls, middlewares)
